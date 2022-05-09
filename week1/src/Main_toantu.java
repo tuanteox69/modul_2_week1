78e7cab5d7bfe677198cbb038716acc15f8412d9
unknown <tuanteox69@gmail.com>
2022-05-09 15:36:54 +0700

    up bai toan tu

---
+++ week1/src/Main_toantu.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main_toantu {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);//Khai báo đối tượng Scanner
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();//Nhập chiều rộng
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();//Nhập chiều dài
+        float area = width * height;
+
+        System.out.println("Area is: " + area);
+    }
+}

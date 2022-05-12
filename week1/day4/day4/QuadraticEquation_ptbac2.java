6a5494182873ffdeb29920a5776871fb170a2fdb
unknown <tuanteox69@gmail.com>
2022-05-12 11:02:22 +0700

    up bai tap

---
+++ week1/day4/day4/QuadraticEquation_ptbac2.java
@@ -0,0 +1,11 @@
+public class QuadraticEquation_ptbac2 {
+
+        private double a;
+        private double b;
+        private double c;
+        private double d =b*2-4*a*c;
+
+    public static void main(String[] args) {
+
+    }
+}

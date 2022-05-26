fdf56420385b2515a53f4189bad41be3f7d6b237
unknown <tuanteox69@gmail.com>
2022-05-26 14:12:07 +0700

    up bai tap

---
+++ week_2/week2/day_4/src/absolute_number_calculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,7 @@
+package absolute_number_calculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        throw new UnsupportedOperationException();
+    }
+}

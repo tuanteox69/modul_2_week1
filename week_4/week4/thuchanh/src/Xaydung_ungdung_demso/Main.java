3f69e911545b2ba29f7756cb74917ecfd502860d
unknown <tuanteox69@gmail.com>
2022-06-01 17:09:14 +0700

    up bai tap

---
+++ week_4/week4/thuchanh/src/Xaydung_ungdung_demso/Main.java
@@ -0,0 +1,21 @@
+package Xaydung_ungdung_demso;
+
+public class Main {
+    public static void main(String args[])
+    {
+        Count count = new Count();
+        try
+        {
+            while(count.getMyThread().isAlive())
+            {
+                System.out.println("Main thread will be alive till the child thread is live");
+                Thread.sleep(1500);
+            }
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("Main thread run is over" );
+    }
+}
